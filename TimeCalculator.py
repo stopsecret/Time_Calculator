9d4c03bfb4c09a089054430c100a1e0b1ed4b8c6
Triangle717 <le717@hotmail.com>
2013-12-24 11:08:27 -0500

    Linting, PEP 8 checks, Python 2 & 3 print and input, Py 2 hashbang

--- TimeCalculator.py
+++ TimeCalculator.py
@@ -1,85 +1,117 @@
+#! /usr/bin/env python
+"""
+    Time Sheet Input Calculator.
+    Copyright 2013 Stopsecret Design.
+    If you enjoyed, please leave a comment on my website.
+    http://stopsecretdesign.wordpress.com/
 
-#################################################################################
-#
-#	Time Sheet Input Calculator. 
-#	Copyright 2013 Stopsecret Design.
-#	If you enjoyed, please leave a comment on my website.
-#	http://stopsecretdesign.wordpress.com/
-#
-#	You are free to use this script for yourself and your personal projects.
-#
-#################################################################################
+    You are free to use this script for yourself and your personal projects.
+"""
+
+from __future__ import print_function
+import sys
+
+# Creates Python 2 and 3 compatible user input
+# For more information, see <http://wp.me/P1V5ge-Y6#input/>
+
+# If this is Python 3, use input()
+if sys.version_info >= (3, 0):
+    get_input = input
+
+# If this is Python 2, use raw_input()
+elif sys.version_info <= (2, 7):
+    get_input = raw_input
 
 
-#The bulk of the processing takes place in this main function, responsible for taking two input strings and outputting time worked in hours
 def hours_calc(inputhoursstart, inputhoursend):
+    """
+    The bulk of the processing takes place in this main function,
+    esponsible for taking two input strings and outputting time worked in hours
+    """
 
-	#if we don't find a colon in the input string, it's not a valid input and we return an error
-	if parsetime(inputhoursstart) == -1 or parsetime(inputhoursend) == -1:
-		return "Error! Please make sure input is in <hour>:<minutes> form and try again!"
-		
-	#otherwise, we get the start time in minutes, and the end time in minutes
-	minstart = parsetime(inputhoursstart)
-	minend = parsetime(inputhoursend)
-	
-	#now, if the end time is less than the start time, we need to add 720 minutes (12 hours) to compensate
-	if(minend < minstart):
-		minend += 720
-		
-	#take the difference of the minutes to see how long the distance between the two is
-	endresult = minend - minstart
-	
-	#we now round to the nearest hour
-	endresulthours = int(endresult/60)
-	
-	#to get the leftover minutes, we multiply by 60 and subtract the original value. We then take the absolute value of this.
-	leftover = abs((endresulthours*60) - endresult)
-	
-	#return a string telling how long we worked
-	return "\n\nYou worked:\n{0}:{1}\n({0} hours and {1} minutes)".format(endresulthours, leftover)
+    # If we don't find a colon in the input string, it's not a valid input
+    # and we return an error
+    if parsetime(inputhoursstart) == -1 or parsetime(inputhoursend) == -1:
+        return "Error! Please make sure input is in <hour>:<minutes> form and try again!"
 
-#parse time returns the time as a minutes value
+    # Otherwise, we get the start time in minutes, and the end time in minutes
+    minstart = parsetime(inputhoursstart)
+    minend = parsetime(inputhoursend)
+
+    # Now, if the end time is less than the start time, we need to add
+    # 720 minutes (12 hours) to compensate
+    if(minend < minstart):
+        minend += 720
+
+    # Take the difference of the minutes to see how long
+    # the distance between the two is
+    endresult = minend - minstart
+
+    # we now round to the nearest hour
+    endresulthours = int(endresult / 60)
+
+    # to get the leftover minutes, we multiply by 60 and subtract
+    # the original value. We then take the absolute value of this.
+    leftover = abs((endresulthours * 60) - endresult)
+
+    # return a string telling how long we worked
+    return '''\nYou worked:
+{0}:{1}
+({0} hours and {1} minutes)'''.format(endresulthours, leftover)
+
+
 def parsetime(inputstring):
-	#if there's no colon, return a value of -1
-	if(inputstring.find(":") == -1):
-		return -1
-	#get the beginning number
-	firstpart = int(inputstring[:inputstring.find(":")])
-	#get the end number
-	secondpart = int(inputstring[(inputstring.find(":") + 1):])
-	#multiply the hours by 60 to turn them into minutes
-	firstpart *= 60
-	#add both these values together
-	endresult = firstpart+secondpart
-	#return the added values
-	return endresult
+    """Returns the time as a minutes value"""
+    # If there's no colon, return a value of -1
+    if(inputstring.find(":") == -1):
+        return -1
+    # Get the beginning number
+    firstpart = int(inputstring[:inputstring.find(":")])
+    # Get the end number
+    secondpart = int(inputstring[(inputstring.find(":") + 1):])
+    # Multiply the hours by 60 to turn them into minutes
+    firstpart *= 60
+    # Add both these values together
+    endresult = firstpart + secondpart
+    # Return the added values
+    return endresult
+
 
 def input_new_hours():
-	#this is the user-end of things for putting in new hours.
-	print("Input hours below:\n")
-	#get both string input values
-	inputstart = raw_input("When did you start working? (H:M)\n\n\n");
-	inputend = raw_input("When did you end working? (H:M)\n\n");
-	#print the results of the calculation
-	print(hours_calc(inputstart, inputend))
+    """This is the user-end of things for putting in new hours."""
+    print("Input hours below:\n")
+    # Get both string input values
+    inputstart = get_input("When did you start working? (H:M)\n\n\n")
+    inputend = get_input("When did you end working? (H:M)\n\n")
+    # Print the results of the calculation
+    print(hours_calc(inputstart, inputend))
 
-#a border to make things look nice
+# A border to make things look nice
 border = "_______________________________________________________________"
-print ("{0}\nTime Sheet Input Calculator. Copyright 2013 Stopsecret Design.\nIf you enjoyed, please leave a comment on my website.\nhttp://stopsecretdesign.wordpress.com/\n{0}\n\n".format(border))
+print ('''{0}
+Time Sheet Input Calculator. Copyright 2013 Stopsecret Design.
+If you enjoyed, please leave a comment on my website.
+http://stopsecretdesign.wordpress.com
+{0}\n\n'''.format(border))
 
-#run the intial solve from the start
+# Run the intial solve from the start
 input_new_hours()
 
-#go into this loop if the user wants to calculate more than one time slot
+# Go into this loop if the user wants to calculate more than one time slot
 while True:
-	new = raw_input("\nDo you want to add another time?\n");
-	#if the user does, redo the calculation
-	if(new.lower() == "yes" or new.lower() == "yeah" or new.lower() == "sure" or new.lower() == "y"):
-		input_new_hours()
-	#if not, then break
-	else:
-		break
-		
-#display a logout message at the end
+    new = get_input("\nDo you want to add another time?\n")
+    # If the user does, redo the calculation
+    if (
+        new.lower() == "yes" or
+        new.lower() == "yeah" or
+        new.lower() == "sure" or
+        new.lower() == "y"
+    ):
+        input_new_hours()
+    # If not, then break
+    else:
+        break
+
+# Display a logout message at the end
 print ("{0}\nThanks for using!\n{0}\n\n".format(border))
-		
+sys.exit(0)
